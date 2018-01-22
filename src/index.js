667d2e9d3d07248eb52063241f62fd3bfb6fd60c
Geoff Davis <geoffdavis92@gmail.com>
2018-01-21 21:53:07 -0600

    removed comments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import "babel-polyfill";
 import { Application } from "stimulus";
-// import AppController from "./controllers/app_controller";
-// import ModalController from "./controllers/modal_controller";
 import TodoController from "./controllers/todo_controller";
 
 const app = Application.start();
 
-// app.register("app", AppController);
-// app.register("modal", ModalController);
 app.register("todo", TodoController);
